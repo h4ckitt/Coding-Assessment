a26d7a235c8346ad4bdcb8b00bf9dd1b5227b7d3
H4ckit <dammysteve10@gmail.com>
2022-01-20 00:04:38 +0100

    Deleted/Renamed Files

--- usecases/repository.go
+++
@@ -1,9 +0,0 @@
-package usecases
-
-import "assessment/domain"
-
-type SchemaRepository interface {
-	Store(car domain.Schema) error
-	GetCarsByColor(color string) ([]domain.Schema, error)
-	GetCarByID(id string) (domain.Schema, error)
-}
