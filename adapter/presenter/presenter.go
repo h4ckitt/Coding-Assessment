e79e528fee8cf2235db117bb2ea2776f0eadf21d
H4ckit <dammysteve10@gmail.com>
2022-01-19 23:37:14 +0100

    Added DTO

---
+++ adapter/presenter/presenter.go
@@ -0,0 +1,8 @@
+package presenter
+
+type Car struct {
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	Color      string `json:"color"`
+	SpeedRange int    `json:"speed_range"`
+}
