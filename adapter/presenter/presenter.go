9d03f6e3e7a2eb95f2e1ad43e2e2b221a239ed66
H4ckit <dammysteve10@gmail.com>
2022-01-20 02:51:57 +0100

    Added Car Feature Field To DTO

--- adapter/presenter/presenter.go
+++ adapter/presenter/presenter.go
@@ -1,8 +1,9 @@
 package presenter
 
 type Car struct {
-	Name       string `json:"name"`
-	Type       string `json:"type"`
-	Color      string `json:"color"`
-	SpeedRange int    `json:"speed_range"`
+	Name       string   `json:"name"`
+	Type       string   `json:"type"`
+	Color      string   `json:"color"`
+	SpeedRange int      `json:"speed_range"`
+	Features   []string `json:"features"`
 }
