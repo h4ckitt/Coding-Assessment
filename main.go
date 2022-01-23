66e4ef7ac4069106ad87b88ff1936b1b40fa0b9e
H4ckit <dammysteve10@gmail.com>
2022-01-23 20:23:59 +0100

    Incorporated gRPC Controller

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	adapter "assessment/adapter/http"
 	"assessment/helpers"
 	"assessment/infrastructure"
-	"assessment/infrastructure/http/router"
+	rpc "assessment/infrastructure/grpc"
+	pb "assessment/infrastructure/grpc/grpc_proto"
+	"assessment/infrastructure/rest/router"
 	db "assessment/repository/postgres"
 	"assessment/usecases"
+	"flag"
 	"fmt"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	"net/http"
 	"os"
 )
@@ -22,11 +27,30 @@ func main() {
 	}
 	usecase := usecases.NewService(repo, logger)
 
-	controller := adapter.NewController(usecase, logger)
+	mode := flag.Bool("grpc", false, "grpc mode")
+	flag.Parse()
 
-	fmt.Println("Starting Server ....")
+	if *mode {
+		lis, err := net.Listen("tcp", ":50051")
 
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), router.InitRouter(controller)); err != nil {
-		log.Panicln(err)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		server := grpc.NewServer()
+		pb.RegisterCarServiceServer(server, rpc.NewGRPCController(usecase, logger))
+
+		if err := server.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+
+		controller := adapter.NewController(usecase, logger)
+
+		fmt.Println("Starting Server ....")
+
+		if err := http.ListenAndServe(":"+os.Getenv("PORT"), router.InitRouter(controller)); err != nil {
+			log.Panicln(err)
+		}
 	}
 }
