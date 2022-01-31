4053e02a3dcd78e6a45e5367135145c07df0535c
H4ckit <dammysteve10@gmail.com>
2022-01-31 17:53:38 +0100

    Added Unit Tests For The Helpers Package

---
+++ helpers/responses_test.go
@@ -0,0 +1,50 @@
+package helpers
+
+import (
+	"assessment/infrastructure"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSuccess(t *testing.T) {
+	InitializeLogger(infrastructure.NewLogger())
+	var codes = []int{200, 201, 202, 204}
+	handler := func(w http.ResponseWriter, r *http.Request, code int) {
+		ReturnSuccess(r, w, code, "Test Returned Success Successfully")
+	}
+
+	req := httptest.NewRequest("GET", "https://area99test.net", nil)
+
+	for _, code := range codes {
+		w := httptest.NewRecorder()
+		handler(w, req, code)
+
+		resp := w.Result()
+
+		if resp.StatusCode != code {
+			t.Errorf("Expected: %v, Got: %v\n", code, resp.StatusCode)
+		}
+	}
+}
+
+func TestFailure(t *testing.T) {
+	InitializeLogger(infrastructure.NewLogger())
+	var codes = []int{400, 401, 402, 403, 404, 405, 500, 503, 504}
+	handler := func(w http.ResponseWriter, r *http.Request, code int) {
+		ReturnFailure(r, w, code, "An Error Occurred")
+	}
+
+	req := httptest.NewRequest("GET", "https://area99test.net", nil)
+
+	for _, code := range codes {
+		w := httptest.NewRecorder()
+		handler(w, req, code)
+
+		resp := w.Result()
+
+		if resp.StatusCode != code {
+			t.Errorf("Expected: %v, Got: %v\n", code, resp.StatusCode)
+		}
+	}
+}
